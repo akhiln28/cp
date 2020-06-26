8eeaa29669431a19380e4eea03e538d9c5ea17ea
akhiln28 <akhil28111997@gmail.com>
2020-06-26 07:13:12 +0530

    Initial commit of educational codeforces round 90

---
+++ ed_90/D.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <climits>
+#include <numeric>
+#include <utility>
+
+#define max_n 200001
+
+int arr[max_n];
+int dp[max_n];
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
