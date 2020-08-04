7a0a729a4d2ad881f1500edd781445e483aac6fa
akhiln28 <akhil28111997@gmail.com>
2020-08-04 16:46:37 +0530

    Clean up commit

--- atcoder/174/E Logs.cpp
+++ atcoder/174/E Logs.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
+#include <algorithm>
+#include <utility>
 #include <vector>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <stdio.h>
+
 using namespace std;
 
-int main() 
+bool isPossible(vector<int> &v, int ans, int k)
 {
-    int N;
-    cin >> N;
+    int sum = 0;
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        int cuts = ceil(v[i] / ans) - 1;
+        sum += cuts;
+    }
+    cout << sum << " " << k << endl;
+    return sum <= k;
+}
+
+int binary_search(vector<int> &v, int k)
+{
+    int max_v = *max_element(v.begin(), v.end());
+    int l = 1, r = max_v;
+    while (abs(l - r) > 1)
+    {
+        cout << l << " " << r << endl;
+        cout << isPossible(v, (l + r) / 2, k) << endl;
+        if (!isPossible(v, (l + r) / 2, k))
+        {
+            l = (l + r) / 2;
+        }
+        else
+        {
+            r = (l + r) / 2;
+        }
+    }
+    return l;
+}
+
+int main()
+{
+    int N, k;
+    cin >> N >> k;
     vector<int> v(N);
     for (size_t i = 0; i < N; i++)
     {
         cin >> v[i];
     }
-    
+    cout << binary_search(v, k) << endl;
     return 0;
 }
