a3d95302e66d4afdd9fb30b7064adf6645bf1ef2
Akhil <akhiln1997@outlook.com>
2021-03-18 08:27:35 +0530

    e throne initial try

---
+++ atcoder/abc_186_e_throne.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+#include <climits>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+    consider the following examples n, s, k
+    example 1: 10, 4, 3
+    the throne is at 0, takahashi is at 4
+    4 -> 7 -> 0 i.e. two steps
+    example 2: 10000, 6, 14
+    6 -> 20 -> ... -> 6 + k * 14 -> 9988 -> 2 first round
+    2 -> ... -> 2 + k * 14 -> 9996 -> 10 second round
+
+    6 -> 2 -> 10 -> 4 -> 12 -> 6
+*/
+
+int main()
+{
+    int num_cases; cin >> num_cases;
+    for (int case_num = 1; case_num <= num_cases; case_num++)
+    {
+    }
+    return 0;
+}
