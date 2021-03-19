db70ca7f2891dd7749517b9e4a513d54a31c37a5
Akhil <akhiln1997@outlook.com>
2021-03-19 08:20:02 +0530

    initial commit

---
+++ leetcode/m_1792_maximum_average_pass_ratio.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+#include <climits>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        auto profit = [&](double passed, double total){
+            return (passed + 1)/(total + 1) - passed/total;
+        };
+        priority_queue<pair<double, vector<int>>> pq;
+        double ans = 0;
+        for (auto &c : classes)
+        {
+            ans += (double) c[0]/c[1];
+            pq.push({profit(c[0], c[1]), {c[0], c[1]}});
+        }
+        while (extraStudents--)
+        {
+            auto [added_profit, c] = pq.top(); pq.pop();
+            ans += added_profit;
+            pq.push({profit(c[0] + 1, c[1] + 1), {c[0] + 1, c[1] + 1}});
+        }
+        return ans/classes.size();
+    }
+};
+
+int main()
+{
+    // {passed, total}
+    vector<vector<int>> classes{{1, 2}, {3, 5}, {2, 2}};
+    int extraStudents = 2;
+    Solution obj;
+    auto ret = obj.maxAverageRatio(classes, extraStudents);
+    cout << ret << endl;
+    return 0;
+}
