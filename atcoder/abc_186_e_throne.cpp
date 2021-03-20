56721810501f34cf57486289d1cfdb7365a21a0f
Akhil <24193184+akhiln28@users.noreply.github.com>
2021-03-20 08:25:00 +0530

    method 1 (not working)

--- atcoder/abc_186_e_throne.cpp
+++ atcoder/abc_186_e_throne.cpp
@@ -17,22 +17,47 @@
 using namespace std;
 
 /*
-    consider the following examples n, s, k
+    consider the following examples n, s (initial position), k (step)
     example 1: 10, 4, 3
     the throne is at 0, takahashi is at 4
     4 -> 7 -> 0 i.e. two steps
     example 2: 10000, 6, 14
     6 -> 20 -> ... -> 6 + k * 14 -> 9988 -> 2 first round
     2 -> ... -> 2 + k * 14 -> 9996 -> 10 second round
-
-    6 -> 2 -> 10 -> 4 -> 12 -> 6
+    each round can be calculated as shown below
+    s + k * count ~ n
+    count = (n - s)/k;
+    new position s = s + k * (count + 1);
 */
 
 int main()
 {
-    int num_cases; cin >> num_cases;
-    for (int case_num = 1; case_num <= num_cases; case_num++)
+    int num_tests;
+    cin >> num_tests;
+    for (int test = 0; test < num_tests; test++)
     {
+        long long n, s, k;
+        cin >> n >> s >> k;
+        int ans = 0;
+        unordered_set<int> vis;
+        vis.insert(s);
+        for (int i = 0; i < 100; i++)
+        {
+            int count = (n - s)/k;
+            s = (s + k * (count)) % n;
+            ans += count;
+            if (s == 0) break;
+            s += k; ans++;
+            cout << s << " " << ans << endl;
+            if (vis.count(s))
+            {
+                ans = -1;
+                break;
+            }
+            vis.insert(s);
+        }
+        cout << ans << endl;
     }
+
     return 0;
 }
