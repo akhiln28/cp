041c894aaa9853527115a7f9e144894d95d6ee8c
Akhil <akhiln1997@outlook.com>
2021-03-20 17:02:06 +0530

    Create m_316_remove_duplicated_letters.cpp

---
+++ leetcode/m_316_remove_duplicated_letters.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+#include <climits>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+    title: m_316_remove_duplicated_letters.cpp
+    author: Akhil
+    date: 2021-03-20
+    time: 09:45:12
+    ==================================================
+    Returning the lexicographically smallest string after removing the duplicated letters.
+
+*/
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+    }
+};
+int main()
+{
+    string str;
+    Solution obj;
+    auto ret = obj.removeDuplicateLetters(str);
+    return 0;
+}
